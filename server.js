c4293ae26ca59fcd9c8cee155b40c4b24bd0d923
emilywheatley-code <emilywheatley@qawolf.com>
2026-03-25 15:10:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const RENTCAST_KEY = process.env.RENTCAST_API_KEY;
